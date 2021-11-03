881471cb6cec9d1a60607d6b326780e10b2d6685
LunST <lunststunt@gmail.com>
2021-11-03 23:45:27 +0300

    set heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,12 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_cable.url = 'wss://https://chactlunst.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [
+    'http://https://chactlunst.herokuapp.com/cable',
+    'https://https://chactlunst.herokuapp.com/cable'
+  ]
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
